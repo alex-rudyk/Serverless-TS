030a55a134db3201745fb333d0c2708f9004ed74
Oleksandr Rudyk <orudyk@impressit.io>
2021-08-26 11:20:38 +0300

    Add doc

--- status-service/app/helpers/dbHelper.ts
+++ status-service/app/helpers/dbHelper.ts
@@ -1,5 +1,35 @@
 import { DynamoDB } from 'aws-sdk';
 
+/**
+ * Create Set UpdateParameters for dynamoDB.
+ * 
+ * @param {string} table table name.
+ * @param {object} keys search keys.
+ * @param {object} attr attributes to update.
+ * @returns {DynamoDB.DocumentClient.UpdateItemInput} params
+ * 
+ * @example
+ *  
+ *  // Create params for updating order name to `Test` in the table `MY_TABLE_NAME` where order uuid is equal `dedc4bfa-e242-43c7-835c-9f32317585bc`.
+ *  const params = createSetUpdateParams('MY_TABLE_NAME', { uuid: dedc4bfa-e242-43c7-835c-9f32317585bc }, { name: 'Test' });
+ *  console.log(params); 
+ *  
+ * output: 
+ * 
+ *  {
+ *      TableName: 'MY_TABLE_NAME',
+ *      Key: {
+ *          uuid: dedc4bfa-e242-43c7-835c-9f32317585bc
+ *      },
+ *      UpdateExpression: 'set #name = :name',
+ *      ExpressionAttributeNames: {
+ *          '#name': 'name'
+ *      },
+ *      ExpressionAttributeValues: {
+ *          ':name': 'Test'
+ *      }
+ *  }
+ */
 export const createSetUpdateParams = (table: string, keys: object, attr: object) => {
     const params: DynamoDB.DocumentClient.UpdateItemInput = {
         TableName: table,
